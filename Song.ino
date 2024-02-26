c894650bc2dc1ebb5d53cf23bf62ec3838b8b794
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-02-26 09:48:36 -0600

    Add files via upload

---
+++ Song.ino
@@ -0,0 +1,22 @@
+int input = 0;
+int speaker = 3;
+int inputRead = 0;
+boolean playing = false;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(input,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+inputRead = digitalRead(input);
+Serial.print("Button =");
+Serial.println(inputRead);
+if(inputRead = HIGH) {
+  boolean playing = true;
+}
+while(playing = true) {
+  tone(speaker,100);
+}
+}
